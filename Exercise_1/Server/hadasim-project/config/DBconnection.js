0981db78a70b5fc1969621efe8493932c1761d2f
TovaLang <toibyla@gmail.com>
2026-08-11 09:13:43 +0300

    Add Docker secrets for MongoDB credentials

--- Exercise_1/Server/hadasim-project/config/DBconnection.js
+++ Exercise_1/Server/hadasim-project/config/DBconnection.js
@@ -1,11 +1,36 @@
 import mongoose from "mongoose";
+import fs from "fs";
+
 export const connectTodb = () => {
-    const mongoURI = process.env.DB_CONNECTION 
-    mongoose.connect(`${mongoURI}`).then(suc => {
-        console.log("mongodb connected on host " + suc.connection.host)
-    }).catch(err => {
+    try {
+        const username = fs.readFileSync(
+            "/run/secrets/mongo_username",
+            "utf8"
+        ).trim();
+
+        const password = fs.readFileSync(
+            "/run/secrets/mongo_password",
+            "utf8"
+        ).trim();
+
+        const mongoURI =
+            `mongodb://${encodeURIComponent(username)}:${encodeURIComponent(password)}@mongo:27017/hadasim?authSource=admin`;
+
+        mongoose.connect(mongoURI)
+            .then(suc => {
+                console.log(
+                    "mongodb connected on host " + suc.connection.host
+                );
+            })
+            .catch(err => {
+                console.log(err);
+                console.log("cannot connect mongodb");
+                process.exit(1);
+            });
+
+    } catch (err) {
+        console.log("cannot read Docker secrets");
         console.log(err);
-        console.log("cannot connect mongodb");
-        process.exit(1)
-    })
-}
+        process.exit(1);
+    }
+};
